bf6ce057907faaf9a0376a24b3e26e5b9bd82246
sternalon <sternalon@gmail.com>
2020-07-14 21:06:30 +0200

    Getting the CSS to work correctly

--- frontend/src/components/card/index.jsx
+++ frontend/src/components/card/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import CardBase from './Card.jsx'
 import ReactDOM from 'react-dom'
 import CardExample from './react-playing-cards/src/CardExample.js'
+import HandExample from './react-playing-cards/src/HandExample.js'
 
 
 
-ReactDOM.render(<CardExample />, document.getElementById('card_component'))
+ReactDOM.render(<HandExample />, document.getElementById('card_component'))
