3e117af04660b7fd6c351f460a23bc61a00165e2
sternalon <sternalon@gmail.com>
2022-03-21 10:58:05 +0200

    Adding unadded files

--- frontend/src/components/series/SeriesBoard.jsx
+++ frontend/src/components/series/SeriesBoard.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import $ from 'jquery'
 import Websocket from 'react-websocket'
 import Hand from "./react-playing-cards/src/PlayingCard/Hand/Hand";
+import GameBoard from './GameBoard'
 // import GameSquare from './GameSquare'
 
 
@@ -14,6 +15,7 @@ class SeriesBoard extends React.Component {
             series: null,
             position: null,
             players: null,
+            current_game: null,
             current_user: props.current_user,
             hand: {
                 cards: ["2d", "2c", "2s", "2h", "2d", "2c", "2s", "2h"],
@@ -25,9 +27,6 @@ class SeriesBoard extends React.Component {
         // bind button click
         this.onCreateGameClick = this.onCreateGameClick.bind(this);
         this.sendSocketMessage = this.sendSocketMessage.bind(this);
-
-        this.isPlayerTurn = this.isPlayerTurn.bind(this)
-
     }
 
 
@@ -43,22 +42,6 @@ class SeriesBoard extends React.Component {
         this.serverRequest.abort();
     }
 
-    _getCardSize() {
-//         console.log("window: ", window.innerWidth);
-//         console.log('handsize', this.state.hand.length)
-//         console.log("size: ", window.innerWidth / this.state.hand.length)
-        let cardSize = window.innerWidth / this.state.hand.cards.length;
-        return this.state.hand.layout !== "spread" || cardSize > 100 ? 100 : cardSize;
-    }
-
-    _getHandPositions() {
-        return {
-            top:{'transform' : 'translateY(-30%) rotate(180deg)', 'right': '50%', 'top': "5%", 'position': 'absolute'},
-            bottom:{'bottom': '-5%', 'right': '50%', 'position': 'absolute', 'height' : '30%'},
-            left:{'bottom': '40%','right': '90%', 'position': 'absolute', 'transform': 'rotate(90deg)' },
-            right:{'bottom': '40%','left': '90%', 'position': 'absolute', 'transform': 'rotate(270deg)' }
-            }
-           }
 
      _getNamePositions() {
         return {
@@ -73,7 +56,7 @@ class SeriesBoard extends React.Component {
     getSeries(){
          const series_url = 'http://127.0.0.1:8000/series-from-id/' + this.props.series_id
          this.serverRequest = $.get(series_url, function (result) {
-            console.log("AAAAAAAA", result)
+            console.log("Result from getSeries API: ", result)
             this.setState({
                 series: result.series,
                 position: this.getPosition(result.series.players),
@@ -106,73 +89,7 @@ class SeriesBoard extends React.Component {
        socket.state.ws.send(JSON.stringify(message))
     }
 
-    isPlayerTurn(){
-        if (this.props.current_user.id == this.state.series.current_turn.id){
-            return true
-        }else{
-            return false
-        }
-    }
 
-
-
-    // ----  RENDER FUNCTIONS ---- //
-    // --------------------------- //
-    renderRow(row_num, cols) {
-
-        let row = cols.map(function(square){
-
-           // for each col, render a square for this row
-           return <GameSquare game_creator={this.state.game.creator.id}
-                              key={square.id}
-                              owner={square.owner}
-                              square_id={square.id}
-                              possession_type={square.status}
-                              loc_x={parseInt(square.col)}
-                              loc_y={parseInt(square.row)}
-                              sendSocketMessage={this.sendSocketMessage}
-                              isPlayerTurn={this.isPlayerTurn}
-                              />
-        }.bind(this))
-
-        return (
-            <tr key={row_num}>{row}</tr>
-        )
-    }
-
-    renderBoard() {
-        // renders the obstruction grid/board
-        // build by row and then by col, based on the height and width values
-        let board = []
-        let cur_row = -1
-        // for each rown
-        if (this.state.game != null && this.state.squares != null){
-            // build the squares
-            // if this is a new row, get the cols
-            board = this.state.squares.map(function(square){
-                if (square.row != cur_row){
-                    // new row
-                    cur_row = square.row
-                    // get just current row cols
-                    let row_cols = this.state.squares.filter(function(c){
-                        if (c.row == cur_row){
-                            return c
-                        }
-                    })
-                    // with array of cols for this row, render it out
-                    //board.push(this.renderRow(cur_row, row_cols))
-                   return this.renderRow(cur_row, row_cols )
-                }
-
-             }, this)
-
-        }else{
-           board = <tr><td>'LOADING...'</td></tr>
-
-        }
-        return board
-
-    }
 
     getPlayerName(position){
         var position_mod_4 = position % 4
@@ -201,7 +118,7 @@ class SeriesBoard extends React.Component {
                        <h2> {this.getPlayerName(player_position+2)}</h2>
                      </div>
 
-                      <div id='right' style={this._getHandPositions().right}>
+                      <div id='right' style={this._getNamePositions().right}>
                              <h2> {this.getPlayerName(player_position+3)}</h2>
                      </div>
                   </div>
@@ -211,84 +128,57 @@ class SeriesBoard extends React.Component {
          }
     }
 
-    renderDealOrLoading() {
-            if (this.state.players){
-                if (this.state.players.length==4){
-                    return (
-                        <div id='center' style={{'top': '50%', 'left': '40%', 'position': 'fixed'}}>
-                        <a className="btn btn btn-danger btn-lg" href="" onClick={this.onCreateGameClick} id="create_game">Click here to Deal!</a>
-                        </div>
-                        )
-                }
-           }
-
-           return (
-          <div id='center' style={{'top': '50%', 'left': '35%', 'position': 'fixed'}}>
-                        <a className="btn btn btn-info btn-lg" href={"."}>Refresh to see if players have joined</a>
-           </div>
+    renderRefreshButton(){
+        return (
+              <div id='center' style={{'top': '50%', 'left': '35%', 'position': 'fixed'}}>
+                            <a className="btn btn btn-info btn-lg" href={"."}>Refresh to see if players have joined</a>
+              </div>
          )
-
     }
 
-    renderDeck() {
 
-      return (
-          <div>
+    renderDealButton(){
+        return (
+              <div id='center' style={{'top': '50%', 'left': '40%', 'position': 'fixed'}}>
+                        <a className="btn btn btn-danger btn-lg" href="" onClick={this.onCreateGameClick} id="create_game">Click here to Deal!</a>
+              </div>
+         )
+    }
 
+    renderGameBoard(){
+        return (
+            <div >
+                    <GameBoard current_user={this.props.current_user} current_game={this.state.series.current_game} socket = {this.props.socket}
+                                 sendSocketMessage={this.sendSocketMessage} />
+            </div>
+        )
+    }
 
-            <div id='top' style={this._getHandPositions().top}>
-                    <Hand hide={true} layout={this.state.hand.layout} cards={this.state.hand.cards} cardSize={1.2*this._getCardSize()}/>
-             </div>
-
-
-            <div id='bottom' style={this._getHandPositions().bottom}>
-                     <Hand hide={false} layout={this.state.hand.layout} cards={this.state.hand.cards} cardSize={1.5*this._getCardSize()}/>
-             </div>
-
-             <div id='left' style={this._getHandPositions().left}>
-                    <Hand hide={true} layout={this.state.hand.layout} cards={this.state.hand.cards} cardSize={0.8*this._getCardSize()}/>
-
-             </div>
-
-              <div id='right' style={this._getHandPositions().right}>
-                    <Hand hide={true} layout={this.state.hand.layout} cards={this.state.hand.cards} cardSize={0.8*this._getCardSize()}/>
-
-             </div>
-
-          </div>
-      );
-  }
-
-    currentTurn(){
-        if (this.state.game){
-            if (this.state.game.completed != null){
-                // game is over
-                return <h3>The Winner: <span className="text-primary">{(this.state.game.current_turn.username)}</span></h3>
+    renderDealOrLoading() {
+        if (this.state.players){
+            if (this.state.players.length<4){
+                return this.renderRefreshButton()
             }else{
-                return <h3>Current Turn:
-                    <span className="text-primary">{(this.state.game.current_turn.username)}</span>
-                 </h3>
+                if (this.state.series != null){
+                    if (this.state.series.current_game == null){
+                        return this.renderDealButton()
+                    }
+                    else{
+                        return this.renderGameBoard()
+                    }
+                }
             }
-
         }
     }
 
 
     render() {
-        console.log("BBBBB", this.props.socket)
-
+        console.log("You are now in the SeriesBoard in series", this.state.series)
         return (
             <div className="row">
 
                    {this.renderNames()}
                    {this.renderDealOrLoading()}
-
-
-
-
-
-                    {this.currentTurn()}
-
 
 
             <Websocket ref="socket" url={this.props.socket}
